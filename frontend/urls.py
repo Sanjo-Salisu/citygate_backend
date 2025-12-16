e1010f8463af2f7b81f52466a87c8db0f26935c8
Sanjo-Salisu <sanjosalisuskillz@gmail.com>
2025-12-16 19:54:12 +0100

    new

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+     path('home/', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('loan/', views.loan, name='loan'),
     path('FAQ/', views.FAQ, name='FAQ'),
