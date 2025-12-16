ee6bff48820a3c99470b28da50edd6b94d990d64
Sanjo-Salisu <sanjosalisuskillz@gmail.com>
2025-12-16 19:17:52 +0100

    new

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('loan/', views.loan, name='loan'),
     path('FAQ/', views.FAQ, name='FAQ'),
